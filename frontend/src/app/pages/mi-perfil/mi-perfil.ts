e36b59f5e61a0287ea6b5b5a237b3c4b370be4e3
moran-eze <e.moran@sistemas-utnfra.com.ar>
2026-06-23 23:47:55 -0300

    Mi perfil

--- frontend/src/app/pages/mi-perfil/mi-perfil.ts
+++ frontend/src/app/pages/mi-perfil/mi-perfil.ts
@@ -1,18 +1,56 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Usuario } from '../../interfaces/usuario.interface';
 import { CommonModule } from '@angular/common';
+import { Publicacion } from '../../interfaces/publicacion.interface';
+import { PublicacionesService } from '../../services/publicaciones.service';
+import { PostCard } from '../../components/post-card/post-card';
 
 @Component({
   selector: 'app-mi-perfil',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PostCard],
   templateUrl: './mi-perfil.html',
   styleUrl: './mi-perfil.css',
 })
-export class MiPerfil {
+export class MiPerfil implements OnInit {
 
-    authService = inject(AuthService);
+  authService = inject(AuthService);
+  publicacionesService = inject(PublicacionesService);
+  private cdr = inject(ChangeDetectorRef);
 
-  usuario: Usuario | null = this.authService.obtenerUsuarioLogueado();
+  usuario: Usuario | null = null;
+
+  ultimasPublicaciones: Publicacion[] = [];
+
+  ngOnInit(){
+    this.usuario = this.authService.obtenerUsuarioLogueado();
+    this.cargarMisPublicaciones();
+  }
+
+  cargarMisPublicaciones(){
+
+      if(!this.usuario) return;
+
+      this.publicacionesService.obtenerPublicaciones()
+      .subscribe({
+
+        next:(publicaciones)=>{
+          this.ultimasPublicaciones = publicaciones
+            .filter(p => p.autorId === this.usuario?._id)
+            .slice(0,3);
+          this.cdr.detectChanges();
+        }
+
+      });
+    }
+
+    eliminarPublicacion(id:string){
+
+      if(!this.usuario) return;
+
+      this.publicacionesService
+        .eliminarPublicacion(id, this.usuario._id!, this.usuario.perfil)
+        .subscribe(()=> this.cargarMisPublicaciones());
+    }
 }
