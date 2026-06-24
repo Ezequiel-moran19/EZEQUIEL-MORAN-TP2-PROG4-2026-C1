56a9156578b57b446b8f25adcb5537e8bdc734a0
moran-eze <e.moran@sistemas-utnfra.com.ar>
2026-06-24 16:59:53 -0300

    Cree el documento Schema y pase la logica ahi en vez de estar en entities

--- backend/src/usuarios/usuarios.module.ts
+++ backend/src/usuarios/usuarios.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Usuario, UsuarioSchema } from './entities/usuario.entity';
+import { Usuario, UsuarioSchema } from './schemas/usuario.schema';
 
 @Module({
   imports: [
