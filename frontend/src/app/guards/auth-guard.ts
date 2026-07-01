654383053970dcf294197fdc145f4830a07a3b40
moran-eze <e.moran@sistemas-utnfra.com.ar>
2026-07-01 06:45:29 -0300

    fix: correccion

--- frontend/src/app/guards/auth-guard.ts
+++ frontend/src/app/guards/auth-guard.ts
@@ -4,36 +4,35 @@ import { AuthService } from '../services/auth.service';
 import { map, catchError, of } from 'rxjs';
 
 export const authGuard: CanActivateFn = () => {
-
   const auth = inject(AuthService);
   const router = inject(Router);
-
-
+  
   if (auth.logueado()) {
     return true;
   }
+  console.log('--- AUTH GUARD ---');
 
+  console.log('logueado signal:', auth.logueado());
 
-  return auth.autorizar().pipe(
-
-    map(usuario => {
-
-      auth.guardarUsuario(usuario);
-      auth.logueado.set(true);
-
-      return true;
-
-    }),
-
-    catchError(() => {
-
-      auth.logueado.set(false);
-      router.navigate(['/login']);
-
-      return of(false);
-
-    })
-
-  );
-
+  const usuarioLocal = auth.obtenerUsuarioLogueado();
+  console.log('usuario local:', usuarioLocal);
+  if (usuarioLocal) {
+     console.log('hay usuario, llamo autorizar');
+    return auth.autorizar().pipe(
+      map(usuario => {
+        auth.guardarUsuario(usuario);
+        auth.logueado.set(true);
+        return true;
+      }),
+      catchError(() => {
+        auth.logout();
+        auth.logueado.set(false);
+        router.navigate(['/login']);
+        return of(false);
+      })
+    );
+  }
+  auth.logueado.set(false);
+  router.navigate(['/login']);
+  return of(false);
 };
