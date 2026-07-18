641d91dd51b7174064703c5264aa1fa5f677c414
Ezequiel Moran <moran.margot@hotmail.com>
2026-07-17 21:53:33 -0300

    Correcciones de main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,31 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+const server = express();
 
 async function bootstrap() {
-
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create( AppModule, new ExpressAdapter(server) );
 
   app.use(cookieParser());
 
-  app.enableCors({
-    origin: process.env.URL_FRONT,
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-    optionsSuccessStatus: 204
-  });
+  app.enableCors({ origin: process.env.URL_FRONT, credentials: true });
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true
-    })
-  );
+  app.useGlobalPipes( new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
-  await app.listen(3000);
+  await app.init();
+
+  return server;
 }
 
-bootstrap();
+export default bootstrap();
