31047eb7b2ca93b63c522a662b712c035b1ba1c3
Ezequiel Moran <moran.margot@hotmail.com>
2026-07-17 22:05:20 -0300

    Correcciones sobre index

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,24 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
-import express from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create( AppModule, new ExpressAdapter(server) );
+ const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser());
-
-  app.enableCors({ origin: ['https://ezequiel-moran-tp-2-prog-4-2026-fro.vercel.app'], credentials: true });
-https://ezequiel-moran-tp-2-prog-4-2026-fro.vercel.app
-  app.useGlobalPipes( new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
-
-  await app.init();
-
-  return server;
+ await app.listen(3000);
 }
 
-export default bootstrap();
+bootstrap();
